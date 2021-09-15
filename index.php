6df1608d2a48b93e7a06bd97681e5e77e3dbb1bf
duong2kkq1 <79644609+duong2kkq1@users.noreply.github.com>
2021-09-15 22:42:17 +0700

    thêm 3 file đầu

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>wow đồ ăn kìa</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class = "logo">
+            <img src="anh/logo.png" alt="Icon WowFood">
+        <div class = "UP">
+            <nav>
+            <a href="" >Home</a>
+            <a href="" >Catetories</a>
+            <a href="" >Foods</a>
+            <a href="" >Contact</a>
+            </nav>
+        </div>
+    </div>
+
+    <div class = "tool">
+        <nav style = "transform: translate(40%, 700%);">
+        <input type="text" name="" id="inputsearch">
+        <button>Search</button>
+        </nav>
+    </div>
+
+    <div class = "ExploreFoods">
+        <p>Explore Foods</p>
+        <div class = "ContentFoods">
+               <img width ="200" src="anh/Pizza_01.jpg" alt="Pizza">
+               <img width ="200" src="anh/Burger_01.jpg" alt="Burger">
+               <img width ="200" src="anh/Momo_01.jpg" alt="Momo">
+        </div>
+    </div>
+
+    <div class = "FoodMenu">
+        <p>Food Menu</p>
+        <div>
+            ????
+        </div>
+        <p>
+            <a href=""> See All Foods</a>
+        </p>
+    </div>
+</body>
+</html>
